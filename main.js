724b640d2ed26e76847c3e8df21829a8c8bded66
Baba-chicharitos <baptiste.ser09@gmail.com>
2024-04-28 21:59:21 +0200

    Commmit 4

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
-const rec1 = document.getElementById("rec1");
-const t1 = document.getElementById("t1");
+const heightWindows = window.innerHeight;
+const widthWindows = window.innerWidth;
+const rectangle2 = document.getElementById("rectangle2");
+let elevenTwelveHeight = (heightWindows / 12) * 11;
+let oneTwelve = heightWindows / 12;
+const widthtRec2 = rectangle2.offsetWidth;
+const heightRec2 = rectangle2.offsetHeight;
+let hypo2 = Math.sqrt(widthtRec2 ** 2 + heightRec2 ** 2);
 
-const widthtRec = rec1.offsetWidth;
-const heightRec = rec1.offsetHeight;
+const angleRad2 = Math.acos(heightRec2 / hypo2);
 
-let hypo = Math.sqrt(widthtRec ** 2 + heightRec ** 2);
+angleDegRec2 = angleRad2 * (180 / Math.PI);
 
-const angleRad = Math.acos(heightRec / hypo);
-
-const angleDeg = angleRad * (180 / Math.PI);
-
+// Appelllllllllllllll Fonctionnnnnnnnnnnnnnnnnnnnnnnnnn
 function rotateStyle() {
-  t1.style.transform = `rotate(${angleDeg}deg)`;
+  t1.style.transform = `rotate(${angleDegRec2}deg)`;
+  t2.style.transform = `rotate(${angleDegRec2}deg)`;
+  t3.style.transform = `rotate(${angleDegRec2}deg)`;
+  t4.style.transform = `rotate(${angleDegRec2}deg)`;
+  console.log(heightWindows, widthWindows);
 }
 
 rotateStyle();
-
-console.log(
-  "hauteur",
-  heightRec,
-  "largeur",
-  widthtRec,
-  "hypoténus",
-  hypo,
-  "angle deg",
-  angleDeg
-);
