2061249ca8166d7cdb54befe61583f611bed6997
Baba-chicharitos <baptiste.ser09@gmail.com>
2024-04-29 00:29:28 +0200

    Commit 5

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const heightWindows = window.innerHeight;
 const widthWindows = window.innerWidth;
+
 const rectangle2 = document.getElementById("rectangle2");
+
 let elevenTwelveHeight = (heightWindows / 12) * 11;
 let oneTwelve = heightWindows / 12;
+
 const widthtRec2 = rectangle2.offsetWidth;
 const heightRec2 = rectangle2.offsetHeight;
+
 let hypo2 = Math.sqrt(widthtRec2 ** 2 + heightRec2 ** 2);
 
 const angleRad2 = Math.acos(heightRec2 / hypo2);
